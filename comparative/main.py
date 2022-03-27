6347991895c2b68f50273c4f43f6291259dfa5ca
Alonso <dac323@lehigh.edu>
2022-03-27 15:36:25 -0400

    Main.py Ready

--- comparative/main.py
+++ comparative/main.py
@@ -1,4 +1,5 @@
 # used to convert the JSON dictionary to a python dictionary object
+from contextlib import nullcontext
 import json
 
 # ComparativeExpressions 
@@ -15,16 +16,29 @@ def comparative_feature_extraction(text):
     with open('comparative_dict.json') as json_file:
         feature_dict = json.load(json_file)
 
-    # TODO clean text and remove punctuation 
+    #Clean text and remove punctuation 
+    clean_text=cleaning_data(text)
 
     # find the number of times that each such key appears in text.
     # loop through all elements in the dictionary.
     # if they are found in the cleaned text, increment the dictionary value.
     for key in feature_dict:
-        if key in text:
+        if key in clean_text:
             feature_dict[key] += 1
 
     return feature_dict
+
+#Clean Text and Remove Punctuation Function
+def cleaning_data(text):
+    print("Orginal: "+text + '\n')
+    clean_text=""
+    #Iterate thorough the review text string
+    for i in range (0,len(text)):
+        if not text[i]=='.':
+            clean_text+=text[i]
+
+    print("New: "+clean_text + '\n')
+    return clean_text
 
 # Driver
 def main():
