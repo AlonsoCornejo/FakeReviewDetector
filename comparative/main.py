f200efddb49f6b19fea70a06d13e93597c9e0ec6
Alonso <dac323@lehigh.edu>
2022-03-27 20:14:03 -0400

    Algo Fully Working, only missing format

--- comparative/main.py
+++ comparative/main.py
@@ -7,9 +7,10 @@ from string import punctuation
 
 # ComparativeExpressions 
 # for a second sub-team
-def comparative_feature_extraction(text):
+def comparative_feature_extraction(file):
     # print input text
-    print(text + '\n')
+    #print(text + '\n')
+    input_str=""
 
     """
         The keys in 'comparative_dict.json' are from 
@@ -18,30 +19,29 @@ def comparative_feature_extraction(text):
     feature_dict = {}
     with open('comparative_dict.json') as json_file:
         feature_dict = json.load(json_file)
-
-
-    #Clean text and remove punctuation 
-    clean_text=cleaning_data(text)
-
-    # find the number of times that each such key appears in text.
-    # loop through all elements in the dictionary.
-    # if they are found in the cleaned text, increment the dictionary value.
-    for key in feature_dict:
-        if key in clean_text:
-            feature_dict[key] += 1
+    
+    #Iterate through line files
+    for x in file:
+        #Clean text and remove punctuation 
+        clean_text=cleaning_data(x)
+        # find the number of times that each such key appears in text.
+        # loop through all elements in the dictionary.
+        # if they are found in the cleaned text, increment the dictionary value.
+        for key in feature_dict:
+            if key in clean_text:
+                feature_dict[key] += 1
 
     return feature_dict
 
 #Clean Text and Remove Punctuation Function
 def cleaning_data(text):
-    print("Orginal: "+text + '\n')
+
     clean_text=""
     #Iterate thorough the review text string
     for i in range (0,len(text)):
         if not shouldBe_erased(text[i]):
             clean_text+=text[i]
 
-    print("New: "+clean_text + '\n')
     return clean_text
 
 #Function to find Punctuation or symbols that should be deleted
@@ -63,7 +63,6 @@ def menu_option():
     print("3: Digital Camera\n\n")
 
     option=input("Please select a product review dataset to analyze: ")
-    print(option)
 
     switcher = {
         1: "MP3_Player.txt",
@@ -78,11 +77,14 @@ def menu_option():
 def main():
     #Main Menu and receive user input
     filename=menu_option()
+
     #Open File to read
     file=open(filename,"r")
-  
-    input_str = 'I prefer apple to samsung|| I prefer ethereum to bitcoin&& I like google the most** Nokia is the best** Lamborghini is on par with Ferrari.'
-    feature_dict = comparative_feature_extraction(input_str)
+    
+
+    feature_dict = comparative_feature_extraction(file)
+    
+    #Print the ocurrences
     for key in feature_dict:
         if feature_dict[key] != 0:
             print(key + ' -> ' + str(feature_dict[key]))
