20d6ad1edf713be805e9c7e84251a12c5d4816b3
Kevin Coxhead <kcc223@lehigh.edu>
2022-04-04 14:50:34 -0400

    HW for 4/4 meeting complete

--- comparative/dataSet.py
+++ comparative/dataSet.py
@@ -30,7 +30,6 @@ class DataSet:
     #Execute the couting of the words
     def comparative_feature_extraction(self):
         #Declare and initialzie some storage variables
-        is_comparative_review = False
         num_comp_review = 0
         num_reviews = 0
         output_file = open("Results.txt", "w")
@@ -42,43 +41,49 @@ class DataSet:
 
         #Iterate through lines of the file
         for x in self.mFile:
-            #Add Review to the count of all reviews
+            # 0 for false, 1 for true
+            is_comparative_review = 0
+
+            # incremement num_reviews each time a new review is acessed
             num_reviews += 1
-            #Clean text and remove punctuation 
+
+            # Clean text and remove punctuation 
             clean_text = self.cleaning_data(x)
 
             #Start writing in the files that will contain the obtained data
-            output_file.write("Review "+str(num_reviews)+":\n")
-            output_file.write(clean_text+"\n")
+            output_file.write("Review " + str(num_reviews) + ":\n")
+            output_file.write(clean_text + "\n")
             # find the number of times that each such key appears in text.
             # loop through all elements in the dictionary.
             # if they are found in the cleaned text, increment the dictionary value.
-            for key in self.feature_dict:
-                
-                if key in clean_text:
-                    #Write the word found in the review
-                    output_file.write("Contain Comparable Word: "+key+"\n")
-                    #Write the word found in the review along with other information regarding the review
+            for (regex, key) in zip(self.feature_dict_compiled, self.feature_dict):
+                num_matches = len(re.findall(regex, clean_text))
+                self.feature_dict[key] += num_matches
+                if num_matches != 0:
+                    is_comparative_review = 1
+
+                    # write detected comparable word to the output file
+                    output_file.write("Contains Comparable Word: "+key+"\n")
+                    # write the word found in the review along with other information regarding the review
                     t_imp.write(str(num_reviews)+","+clean_text+","+str(1)+","+key+"\n")
-                    #Add ocurrence to the recorded list of ocurrences
-                    feature_dict[key] += 1
-                    is_comparative_review=True
+
+            self.classification.append(is_comparative_review)
                     
             #Get Number of comparative reviews
             if is_comparative_review:
-                num_comp_review+=1
+                num_comp_review += 1
             else:
                 #Write that a comparative word was not found in the review
-                t_imp.write(str(num_reviews)+","+clean_text+","+str(0)+","+"None"+"\n")
+                t_imp.write(str(num_reviews) + "," + clean_text + "," + str(0) + "," + "None" + "\n")
                 output_file.write("Does NOT Contain Comparable Words\n")
             #Set false for next review analysis
             is_comparative_review=False
 
             #Print line delimiting end of review
-            output_file.write("*****************************************************************\n")
+            output_file.write("**********************************************\n")
         output_file.close()
         t_imp.close()
-        return feature_dict,num_comp_review,num_reviews
+        return num_comp_review, num_reviews
 
     #Print Summary of Search
     def get_summary(self):
@@ -88,9 +93,8 @@ class DataSet:
         
         #Access obtained Values from identification algorithm
         filename= self.filename
-        feature_dict= result[0]
-        num_CompRev= result[1]
-        num_reviews= result[2]
+        num_comp_review= result[0]
+        num_reviews= result[1]
 
         #Get Product Name
         _position=filename.find('_')
@@ -98,25 +102,32 @@ class DataSet:
 
         #Get Sum of All Comparable Words
         sum_compwords=0
-        for key in feature_dict:
-            sum_compwords+=feature_dict[key]
+        for key in self.feature_dict:
+            sum_compwords += self.feature_dict[key]
         
         #Print Basic Information and findings
-        print("\n************************************************************************************")
+        print("\n******************************************************")
         print('\033[1m'+'\033[4m'+"General Summary: \n"+'\033[0m')
         print("Product: "+ product_name)
         print("Data file: "+ filename)
         print("Number of Reviews: "+ str(num_reviews))
         print("Number of Comparabale Words: "+ str(sum_compwords))
-        print("Number of Reviews containing comparable words: "+ str(num_CompRev))
+        print("Number of Reviews containing comparable words: "+ str(num_comp_review))
         print("Average comprable words per Review: "+ str(round(sum_compwords/num_reviews,2)))
-        print("Average comprable words per Comparable Review: "+ str(round(sum_compwords/num_CompRev,2)))
-        print("Percentage of Comparable reviews from all reviews: " + str(round((num_CompRev/num_reviews)*100,2)) + "%")
+        print("Average comprable words per Comparable Review: "+ str(round(sum_compwords/num_comp_review,2)))
+        print("Percentage of Comparable reviews from all reviews: " + str(round((num_comp_review/num_reviews)*100,2)) + "%\n")
         
         #Print the ocurrences
         print('\033[1m'+'\033[4m'+"All Comparable Word Ocurrances: \n"+'\033[0m')
-        for key in feature_dict:
-            if feature_dict[key] != 0:
-                print(key + ' -> ' + str(feature_dict[key]))
+        for key in self.feature_dict:
+            if self.feature_dict[key] != 0:
+                print(key + ' -> ' + str(self.feature_dict[key]))
         
-        print("***********************************************************************************\n")
+        print("******************************************************\n")
+    
+    # closes the dataset file
+    def mFileClose(self):
+        if not self.mFile.closed:
+            self.mFile.close()
+            return
+        print("File is already closed")
