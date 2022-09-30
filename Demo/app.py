399c629992260008a21c112c9e95c44454744808
Alonso <dac323@lehigh.edu>
2022-09-30 10:20:01 -0400

    POST/GET Basics

--- Demo/app.py
+++ Demo/app.py
@@ -1,18 +1,25 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template,request
+from werkzeug.utils import redirect
 
 app= Flask(__name__)
 
 @app.route("/")
-def home():
+def Home():
     return render_template("main.html")
 
-@app.route("/test", methods=["POST","GET"])
-def login():
-    return render_template("test.html")
+@app.route("/Success/<input>")
+def Updated(input):
+    return 'You inserted: %s' % input
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route('/get_Input',methods=['POST','GET'])
+def get_Input():
+    if request.method== 'POST':
+        product=request.form["Product"]
+        return redirect(url_for("Updated",input=product))
+    else:
+        product=request.args.get('Product')
+        return redirect(url_for("Updated",input=product))
 
 if __name__=="__main__":
     app.run(debug=True)
+    
