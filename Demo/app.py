a5a0e0542e240d21a2f36d85c4f668fb2a631fb3
Kamilla Muminova <kam423@lehigh.edu>
2022-10-05 23:50:21 -0400

    created both the tables as well as magic methods to represent them, and wrote in readme more info

--- Demo/app.py
+++ Demo/app.py
@@ -2,6 +2,7 @@
 #from Demo.scrapping import Scrapping
 from flask import Flask, redirect, url_for, render_template,request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
 from werkzeug.utils import redirect
 import scrapping
 
@@ -9,14 +10,29 @@ app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-class product(db.Model):
+class Product(db.Model):
+    _tablename_ = "product"
     id = db.Column(db.Integer, primary_key=True)                                #primary key that identifies the product
     image_file = db.Column(db.Text, nullable = True, default='default.jpg')     #store the string path to image of product to use in html later, can be empty, needs to be hashed?
     descript = db.Column(db.Text, nullable = True)                              #store the product description, can be empty
-    product_url = db.Column(db.Text, unique = True, nullable = False)           #store the product url, cannot be empty since we need a url to the product
+    prod_url = db.Column(db.Text, unique = True, nullable = False)              #store the product url, cannot be empty since we need a url to the product
     
-    
+    def __repr__(self):                                                         #dunder method or magic method to represent database when its printed out
+        return f"Product('{self.image_file}','{self.descript}','{self.prod_url}')"
 
+class Review(db.Model):
+    _tablename_ = "review"
+    id = db.Column(db.Integer, primary_key=True)                                #primary key that identifies the product
+    prod_id = db.Column(db.Integer, ForeignKey("product.id"))                   #store which product id this review is associated with, using it as a foreign key
+    chart1 = db.Column(db.Text, nullable = False)                               #store the jpg of the chart associated with this review based on algorithm result
+    alg1 = db.Column(db.Text, nullable = False)                                 #store result of first algorithm
+    chart2 = db.Column(db.Text, nullable = False)                               #store the jpg of the chart associated with this review based on algorithm result
+    alg2 = db.Column(db.Text, nullable = False)                                 #store result of first algorithm
+    
+    def __repr__(self):                                                         #dunder method or magic method to represent database when its printed out
+        return f"Product('{self.prod_id}','{self.chart1}','{self.alg1}','{self.chart2}','{self.alg2}')"
+
+    
 @app.route("/")
 def Home():
     return render_template("main.html")
--- Demo/readme.txt
+++ Demo/readme.txt
@@ -1,5 +1,5 @@
 For the database:
 
     -> Table that will hold the product id (primary key), product image, product description, product url 
-    -> Table that will hold review id (primary key), product id, chart 1 associated with review, algorithm 1 result, chart 2 associated with       review, algorithm 2 result. 
+    -> Table that will hold review id (primary key), product id, chart 1 associated with review, algorithm 1 result, chart 2 associated with review, algorithm 2 result. 
         -> Multiple reviews can relate to one product id. 
